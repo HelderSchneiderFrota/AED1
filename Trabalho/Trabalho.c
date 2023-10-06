dae11b622e52c1022aca324c29d88100fe2c22f8
Helder Schneider Frota <helder.schneider.frota@gmail.com>
2023-10-06 10:10:16 -0400

    Update Trabalho.c

--- Trabalho/Trabalho.c
+++ Trabalho/Trabalho.c
@@ -1,35 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 double Estoque[10], Clientes[10];
-
-
-void limpatela(){
-    
-#ifdef _WIN32
-        system("cls"); // Para Windows
-    #else
-        system("clear"); // Para Unix/Linux
-    #endif
-}
 
 void ConsutarCrediario(){
     int CodigoCliente;
     printf("Digite o Codigo do cliente : \n");
     scanf("%d", &CodigoCliente);
     printf("O Saldo em aberto do Cliente eh: %lf\n", Clientes[CodigoCliente]);
+    sleep(2);
     MENU();
 }
 
 void PreencherEstoque(int d, int f){
     Estoque[d] += f;
     printf("Estoque preenchido\n");
+    sleep(2);
     MENU();
 }
 
-void PagamentoCrediario(){
-
+void PagamentoCrediario(int a){ // a é o codigo do cliente
+    float pagamentocliente;
+    printf("O Saldo em aberto do Cliente eh: %lf\n", Clientes[a]);
+    printf("Digite o valor que deseja pagar: ");
+    scanf("%f", &pagamentocliente);
+        if(Clientes[a] >= pagamentocliente){
+            Clientes[a] -= pagamentocliente;
+            printf("Pagamento realizado com sucesso!! \n");
+            printf("O valor no crediario ficou: %lf", Clientes[a]);
+            sleep(2);
+            MENU();
+        }
+        else{
+            printf("Por favor, faça pagamentos somente no valor total ou parcial \n");
+            PagamentoCrediario(a);
+        }
 }
 
 void Pagamento(float VALOR){
@@ -71,6 +78,7 @@ void Pagamento(float VALOR){
 void tabelaItens(){
     int Quantidade=0, Produto, Soma=0;
     float  Valor;
+    sleep(1);
     printf("Codigo\t\t   Item \t\t Preco \n");
     printf("\n");
     printf("1\t  Barrinha de proteina \t\t 04.50 R$ \n");
@@ -84,7 +92,6 @@ void tabelaItens(){
     printf("9\t  BCAA                 \t\t 12.50 R$\n");
     printf("10\t Omega 3               \t\t 89.00 R$\n");
     printf("0\t Finalizar compra\n");
-    
     printf("Digite o codigo referente ao produto que deseja: ");
     scanf("%d", &Produto);
     do{
@@ -244,65 +251,81 @@ Pagamento(Valor);
 
 void sorteio(){
     srand(time(0));
-    printf("\n Aqui está um numero sorteado :%d ", rand()%10);
+    int aleatorio =  rand()%10;
+    Clientes[aleatorio] -=  Clientes[aleatorio];
+    printf("\n O cliente sortudo foi o referente ao codigo:%d e teve sua divida perdoada!!\n", aleatorio);
+    sleep(2);
+    MENU();
 }
 
 void ConsultarEstoque(){
-    int i;
     printf("Quantidade de produtos no estoque: \n");
-        for(i=0;i<=10;i++){
+        for(int i=0;i<=9;i++){
             printf("Codigo: %d, Quantidade: %.2lf  \n",i, Estoque[i]);
         }
+    sleep(2);    
     MENU();
 }
 
 void MENU(){
-    int men,a,D,F;
+    int men,a,D,F,c;
+    sleep(1);
     printf("\t\t\tMenu\n");
     printf("O que deseja fazer?\n");
     printf("\n");
     printf("1\t Simular compra\n");//PRONTO!!!
-    printf("2\t Consultar debidos de clientes do Crediario\n");//PARECE TA PRONTO
+    printf("2\t Consultar debidos de clientes do Crediario\n");//ok
     printf("3\t Preencher estoque\n");//OK
     printf("4\t Consultar estoque\n");//ok
-    printf("5\t Pagar debitos do clientes do Crediario (total ou parcial)\n");
-    printf("6\t Sortear cliente que terá sua divida do crediario perdoada\n");
+    printf("5\t Pagar debitos do clientes do Crediario (total ou parcial)\n");//ok
+    printf("6\t Sortear cliente que terá sua divida do crediario perdoada\n");//ok
     printf("\n");
     printf("Digite o numero referente ao opcao: \n");
     scanf("%d", &men);
-    switch (men){
-    case 1:
-        tabelaItens();
-        break;
-    case 2:
-        ConsutarCrediario();
-        break;
-    case 3:
-        printf("Digite o codigo do produto para preencher o estoque: \n");
-        scanf("%d", &D);
-        printf("Digite a quantidade: \n");
-        scanf("%d", &F);
-        PreencherEstoque(D,F);
-        break;
-    case 4:
-        ConsultarEstoque();
-        break;
-    case 5:
-        printf("digite o codigo do cliente: \n");
-        scanf("%d", &a);
-        PagamentoCrediario(a);
-        break;
-    case 6:
-        sorteio();
-        break;
-    default:
-        printf("Codigo invalido");
-        break;
-    }
+        switch (men){
+            case 1:
+                tabelaItens();
+                break;
+            case 2:
+                ConsutarCrediario();
+                break;
+            case 3:
+                printf("Deseja preencher todo o estoque com 10 produtos? 1 para sim, 2 para nao: ");
+                scanf("%d", &c);
+                    if(c != 1){
+                        printf("Digite o codigo do produto para preencher o estoque: \n");
+                        scanf("%d", &D);
+                        printf("Digite a quantidade: \n");
+                        scanf("%d", &F);
+                        PreencherEstoque(D,F);
+                    }
+                    else{
+                        for(int i=0; i<=9; i++){
+                        Estoque[i] += 10;
+                        }
+                    printf("Estoque preenchido\n");
+                    MENU();
+                    }
+                break;
+            case 4:
+                ConsultarEstoque();
+                break;
+            case 5:
+                printf("digite o codigo do cliente: \n");
+                scanf("%d", &a);
+                PagamentoCrediario(a);
+                break;
+            case 6:
+                sorteio();
+                break;
+            default:
+                printf("Codigo invalido");
+                MENU();
+                break;
+        }
 }
 
 int main(){
-    //limpatela();
     printf("\tSeja Bem vindo ao Helder e Kaue suplementos\n ");
     printf("\n");
 
